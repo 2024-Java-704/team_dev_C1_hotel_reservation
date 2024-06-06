07edf6170bd99ddef7f790fd952dabf6785af9a5
inage-hikaru-7322 <420-student074@la-study.com>
2024-06-06 09:11:55 +0900

    first commit

---
+++ src/main/java/com/example/demo/TeamDevC1HotelReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDevC1HotelReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDevC1HotelReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/TeamDevC1HotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDevC1HotelReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
