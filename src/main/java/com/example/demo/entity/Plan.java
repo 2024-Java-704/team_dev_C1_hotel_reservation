b4baf3fd00e786a0f8bc8a4dffd4ce1cd5789be7
SasamoriYuna <420-student035@la-study.com>
2024-06-07 11:15:32 +0900

    PlanEntityの完成

--- src/main/java/com/example/demo/entity/Plan.java
+++ src/main/java/com/example/demo/entity/Plan.java
@@ -1,6 +1,17 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "plans")
 public class Plan {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Integer innId;
 	private String name;
