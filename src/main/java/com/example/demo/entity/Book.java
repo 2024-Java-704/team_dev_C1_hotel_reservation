59d90dac7fe6be5231f9d525b1038968ba5a3480
saji-akane-7696 <420-student060@la-study.com>
2024-06-07 10:30:20 +0900

    Entity作成済み(Book)

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -1,5 +1,84 @@
 package com.example.demo.entity;
 
-public class Book {
+import java.sql.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer paymentId;
+	private Integer userId;
+	private Integer planId;
+	private Integer adultNum;
+	private Integer childNum;
+	private Date bookingDate;
+	private Date inDate;
+	private Date outDate;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(Integer paymentId) {
+		this.paymentId = paymentId;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getPlanId() {
+		return planId;
+	}
+	public void setPlanId(Integer planId) {
+		this.planId = planId;
+	}
+	public Integer getAdultNum() {
+		return adultNum;
+	}
+	public void setAdultNum(Integer adultNum) {
+		this.adultNum = adultNum;
+	}
+	public Integer getChildNum() {
+		return childNum;
+	}
+	public void setChildNum(Integer childNum) {
+		this.childNum = childNum;
+	}
+	public Date getBookingDate() {
+		return bookingDate;
+	}
+	public void setBookingDate(Date bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+	public Date getInDate() {
+		return inDate;
+	}
+	public void setInDate(Date inDate) {
+		this.inDate = inDate;
+	}
+	public Date getOutDate() {
+		return outDate;
+	}
+	public void setOutDate(Date outDate) {
+		this.outDate = outDate;
+	}
+	
+	
 }
+ 
