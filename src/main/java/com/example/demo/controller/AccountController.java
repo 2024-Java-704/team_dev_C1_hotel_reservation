30163f12a973f8c650c0ba644d20720b11c19242
inage-hikaru-7322 <420-student074@la-study.com>
2024-06-07 10:59:38 +0900

    AccountControllerの作成

---
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -0,0 +1,74 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class AccountController {
+	@Autowired
+	HttpSession session;
+
+	@GetMapping({ "/login", "/logout" })
+	public String index(String error, Model model) {
+		session.invalidate();
+
+		return "";
+	}
+
+	@PostMapping("/login")
+	public String login(String email, String password, Model model) {
+		return "";
+	}
+
+	@GetMapping("/users/create")
+	public String createUser() {
+		return "";
+	}
+
+	@PostMapping("/users/create")
+	public String addUser(String name, String adress, String tel, String email, String zipCode, String password,
+			Model model) {
+		return "";
+	}
+
+	@GetMapping("/mypage")
+	public String menuUser() {
+		return "";
+	}
+
+	@GetMapping("/mypage/users/{id}/edit")
+	public String editUser(Integer id) {
+		return "";
+	}
+
+	@PostMapping("/mypage/users/{id}/edit")
+	public String updateUser(Integer id, String name, String adress, String tel, String email, String zipCode,
+			String password, Model model) {
+		return "";
+	}
+
+	@PostMapping("/mypage/users/{id}/delete")
+	public String deleteUser(Integer id) {
+		return "";
+	}
+
+	@GetMapping("/mypage/booked")
+	public String detailBook() {
+		return "";
+	}
+
+	@PostMapping("/mypage/booked/{id}/cancel")
+	public String cancelBook(Integer id) {
+		return "";
+	}
+
+	@GetMapping("/mypage/booked/history")
+	public String logBook() {
+		return "";
+	}
+}
