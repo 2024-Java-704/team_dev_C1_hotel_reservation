0c6dd6142e2d335b57bfe037cee8752288459719
inage-hikaru-7322 <420-student074@la-study.com>
2024-06-07 13:48:49 +0900

    大枠完成

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -23,11 +23,12 @@ public class Book {
 	private Date inDate;
 	private Date outDate;
 	private Integer innId;
-	
+
 	public Book() {
 	}
-	
-	public Book(Integer paymentId,Integer userId,Integer planId, Integer adultNum, Integer childNum, Date bookingDate, Date inDate ,Date outDate) {
+
+	public Book(Integer paymentId, Integer userId, Integer planId, Integer adultNum, Integer childNum, Date bookingDate,
+			Date inDate, Date outDate, Integer innId) {
 		this.paymentId = paymentId;
 		this.userId = userId;
 		this.planId = planId;
@@ -38,64 +39,83 @@ public class Book {
 		this.outDate = outDate;
 		this.innId = innId;
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getPaymentId() {
 		return paymentId;
 	}
+
 	public void setPaymentId(Integer paymentId) {
 		this.paymentId = paymentId;
 	}
+
 	public Integer getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
+
 	public Integer getPlanId() {
 		return planId;
 	}
+
 	public void setPlanId(Integer planId) {
 		this.planId = planId;
 	}
+
 	public Integer getAdultNum() {
 		return adultNum;
 	}
+
 	public void setAdultNum(Integer adultNum) {
 		this.adultNum = adultNum;
 	}
+
 	public Integer getChildNum() {
 		return childNum;
 	}
+
 	public void setChildNum(Integer childNum) {
 		this.childNum = childNum;
 	}
+
 	public Date getBookingDate() {
 		return bookingDate;
 	}
+
 	public void setBookingDate(Date bookingDate) {
 		this.bookingDate = bookingDate;
 	}
+
 	public Date getInDate() {
 		return inDate;
 	}
+
 	public void setInDate(Date inDate) {
 		this.inDate = inDate;
 	}
+
 	public Date getOutDate() {
 		return outDate;
 	}
+
 	public void setOutDate(Date outDate) {
 		this.outDate = outDate;
 	}
+
 	public Integer getInnId() {
 		return innId;
 	}
+
 	public void setInnId(Integer innId) {
 		this.innId = innId;
 	}
