085d29972adf31745a8a852354d70f1823e83b42
tsuyuki-taiyo-6375 <420-student037@la-study.com>
2024-06-17 10:52:51 +0900

    ripojitory

---
+++ src/main/java/com/example/demo/repository/AvailabilityRepository.java
@@ -0,0 +1,12 @@
+//package com.example.demo.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface AvailabilityRepository extends JpaRepository<Availability, Integer>{
+//	findByInnId(Integer innId);
+//	
+//	findByNum(Integer num);
+//	
+//	findByInnIdAndNum(Integer innId, Integer num);
+//
+//}
