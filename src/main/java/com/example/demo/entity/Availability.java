811ecb86195f24bf4d36c73b211d99b9cf9cd96c
tsuyuki-taiyo-6375 <420-student037@la-study.com>
2024-06-17 10:32:49 +0900

    entity

---
+++ src/main/java/com/example/demo/entity/Availability.java
@@ -0,0 +1,61 @@
+//package com.example.demo.entity;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "availabilities")
+//public class Availability {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Integer id;
+//
+//	@Column(name = "inn_id")
+//	private Integer innId;
+//
+//	private Integer num;
+//
+//	Availability() {
+//
+//	}
+//
+//	Availability(Integer id, Integer innId, Integer num) {
+//		this.id=id;
+//		this.innId=innId;
+//		this.num=num;
+//	}
+//	
+//	Availability(Integer innId, Integer num) {
+//		this.innId=innId;
+//		this.num=num;
+//	}
+//
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public Integer getInnId() {
+//		return innId;
+//	}
+//
+//	public Integer getNum() {
+//		return num;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public void setInnId(Integer innId) {
+//		this.innId = innId;
+//	}
+//
+//	public void setNum(Integer num) {
+//		this.num = num;
+//	}
+//
+//}
---
+++ src/main/java/com/example/demo/entity/Prefecture.java
@@ -0,0 +1,42 @@
+//package com.example.demo.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "prefectures")
+//public class Prefecture {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Integer id;
+//	private String name;
+//
+//	Prefecture() {
+//
+//	}
+//
+//	Prefecture(String name) {
+//		this.name=name;
+//
+//	}
+//
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//}
