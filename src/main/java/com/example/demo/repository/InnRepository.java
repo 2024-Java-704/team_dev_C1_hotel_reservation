3eacdf5da00ccfc74600f67c13f4b663f7927296
inage-hikaru-7322 <420-student074@la-study.com>
2024-06-19 11:59:06 +0900

    EditInnController

--- src/main/java/com/example/demo/repository/InnRepository.java
+++ src/main/java/com/example/demo/repository/InnRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.entity.Inn;
 
@@ -27,10 +29,6 @@ public interface InnRepository extends JpaRepository<Inn, Integer> {
 
 	List<Inn> findByNameContainingOrAddressContaining(String keyword, String keyword2);
 
-	
-
-	
-
-
-
+	@Query("SELECT e FROM Inn e WHERE e.id = (SELECT MAX(e2.id) FROM Inn e2)")
+	Optional<Inn> findLastInserted();
 }
